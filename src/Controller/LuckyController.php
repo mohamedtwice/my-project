9b547f0c889f450f56f5e05f7d5d799d8687e7e6
Mohamed <mhm@umn.edu>
2018-01-09 16:01:52 -0600

    routing basics

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,19 +4,25 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Psr\Log\LoggerInterface;
 
-class LuckyController
+class LuckyController extends Controller
 {
-     /**
-      * @Route("/lucky/number")
-      */
-    public function number()
-    {
-        $number = mt_rand(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+  public function numberAction($max, LoggerInterface $logger)
+  {
+      $logger->info('We are logging!');
+      // ...
+
+    // public function number($max)
+    // {
+      $number = mt_rand(0, $max);
+
+        return $this->render('lucky/number.html.twig', array(
+          'number' => $number,
+        ));
     }
+
+
 }
